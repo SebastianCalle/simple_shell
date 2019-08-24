51954b3de703595bc67d258b0d9f844c8df5d21b
Juan Sebastian Calle <jusemonca@gmail.com>
2019-08-24 18:25:36 -0500

    [Fix]: Betty problems with files _print_list.c _proexec.c

--- _print_list.c
+++ _print_list.c
@@ -1,12 +1,16 @@
 #include "shell.h"
-
+/**
+ * _print_list - function  that print list
+ * @head: node to print
+ * Return: the number of nodes
+ */
 int _print_list(node_t *head)
 {
 	int i;
 
 	for (i = 0; head; i++)
 	{
-	        _puts(head->s);
+		_puts(head->s);
 		_putchar(10);
 		head = head->next;
 	}
--- _proexec.c
+++ _proexec.c
@@ -1,28 +1,19 @@
 #include "shell.h"
-
-int _proexec(char **argv)
+/**
+ * temp_function - function that execute the nodes
+ * @temp: node temporal
+ * @status: status of program
+ * @argv: arguments to execute
+ * @path: path of directory
+ * Return: status of program
+ */
+int temp_function(node_t *temp, int *status, char **argv, char *path)
 {
-	struct dirent *fil = NULL;
 	DIR *dir = NULL;
-	int status, i, j, len, y = 0;
-	node_t *temp = NULL;
-	char *buff, *path = _getenv("PATH"), **tok = _strtok(path, &y), *slash = "/";
+	struct dirent *fil = NULL;
+	int len, i;
+	char *buff = NULL, *slash = "/";
 
-	i = _strcmp(argv[0], "env"), j = _strcmp(argv[0], "setenv");
-	if (argv[0][0] == '/')
-	{
-		status = execve(argv[0], argv, environ);
-		return (status);
-	}
-	else if (i == 0 || j == 0 || _strcmp(argv[0], "unsetenv") == 0)
-		return (0);
-	for (i = 0; tok[i]; i++)
-	{
-		if (path[0] == ':' && i == 0)
-			_add_node_end(&path_s, _getenv("PWD"));
-		_add_node_end(&path_s, tok[i]);
-	}
-	temp = path_s;
 	while (temp)
 	{
 		dir = opendir(temp->s);
@@ -40,12 +31,42 @@ int _proexec(char **argv)
 					buff[i] = 0;
 				buff = _strcat(buff, temp->s), buff = _strcat(buff, slash);
 				buff = _strcat(buff, fil->d_name);
-				status = execve(buff, argv, environ);
+				*status = execve(buff, argv, environ);
 				closedir(dir), free(buff), free(path);
-				return (status);
+				return (*status);
 			}
 		}
 		temp = temp->next;
 	}
+	return (0);
+
+}
+/**
+ * _proexec - function that execute the arguments
+ * @argv: arguments parameters
+ * Return: status or -1 if not succes
+ */
+int _proexec(char **argv)
+{
+	int status, i = 0, j, y = 0;
+	node_t *temp = NULL;
+	char *path = _getenv("PATH"), **tok = _strtok(path, &y);
+
+	i = _strcmp(argv[0], "env"), j = _strcmp(argv[0], "setenv");
+	if (argv[0][0] == '/')
+	{
+		status = execve(argv[0], argv, environ);
+		return (status);
+	}
+	else if (i == 0 || j == 0 || _strcmp(argv[0], "unsetenv") == 0)
+		return (0);
+	for (i = 0; tok[i]; i++)
+	{
+		if (path[0] == ':' && i == 0)
+			_add_node_end(&path_s, _getenv("PWD"));
+		_add_node_end(&path_s, tok[i]);
+	}
+	temp = path_s;
+	temp_function(temp, &status, argv, path);
 	return (-1);
 }
