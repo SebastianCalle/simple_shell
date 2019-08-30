cdcb3e43db91b7fbc5fc4cedc14c26fc0e909b1f
Hugo Vargas <vargas88hugo@gmail.com>
2019-08-30 18:18:12 -0500

    [update] reset before error

--- execute.c
+++ execute.c
@@ -1,4 +1,5 @@
 #include "shell.h"
+
 /**
  * execute - function that execute the commands
  * @args: commands to execute
@@ -10,13 +11,22 @@
 int execute(char **args, char *line, node_t **env_s, node_t **path_s)
 {
 	pid_t pid;
-	int status, status2;
+	int status, status2, i;
+	char str[1024];
+
+	for (i = 0; args[0][i]; i++)
+		;
+	_strcpy(str, args[0]);
 
 	pid = fork();
 	if (pid == 0)
 	{
 		if ((_proexec(args, line, env_s, path_s)) == -1)
-			perror("sh");
+		{
+			write(STDERR_FILENO, "./hsh: 1: ", 10);
+			write(STDERR_FILENO, str, i);
+			write(STDERR_FILENO, ": not found\n", 12);
+		}
 		exit(127);
 	}
 	else if (pid < 0)
